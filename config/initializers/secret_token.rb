6393ed9e05384e07964f14a8b5a0c5748b55d670
Patrick E. Burke <uconnfootball75@yahoo.com>
2012-11-07 21:04:49 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SixthApp::Application.config.secret_token = '52200594d4f0bf30890e9fb0a9e27b7bbe09cd3f69fbb0c8753e22cca79c82c93ff2920677eeb7949ea8c59f49ef652628f4d5e15e8bb657329815363e90526d'
